5baf410b10a8f291353ed690292a02c27e7ff873
fulya_ture <fulyature@gmail.com>
2023-11-03 22:50:14 +0300

    gelirler event

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 //?SELECTORS
 
+const gelirInput = document.getElementById("gelir-input");
+const ekleBtn = document.getElementById("ekle-btn");
+const ekleFormu = document.getElementById("ekle-formu");
+
 //?VARİABLES
 
+let gelirler = "";
+
 //?EVENTS
+
+// formun submıt btn a basıldıgında otomatı kolarak bu event e gelır
+ekleFormu.addEventListener("submit", (e) => {
+  e.preventDefault();
+  gelirler += gelirInput.value;
+  console.log(gelirler);
+});
 
 //?FUNCTİONS
